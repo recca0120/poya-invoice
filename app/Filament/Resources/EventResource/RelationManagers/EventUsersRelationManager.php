29799cea86212df1f266f3592ef53b1a1212ebd7
recca0120 <recca0120@gmail.com>
2024-05-09 02:41:00 +0800

    show more info

--- app/Filament/Resources/EventResource/RelationManagers/EventUsersRelationManager.php
+++ app/Filament/Resources/EventResource/RelationManagers/EventUsersRelationManager.php
@@ -39,6 +39,8 @@ class EventUsersRelationManager extends RelationManager
             ->columns([
                 Tables\Columns\TextColumn::make('event.code')->label('活動代號'),
                 Tables\Columns\TextColumn::make('user.name')->label('姓名'),
+                Tables\Columns\TextColumn::make('user.member_card_number')->label('會員卡號'),
+                Tables\Columns\TextColumn::make('user.phone_number')->label('手機號碼'),
                 Tables\Columns\TextColumn::make('sn')
                     ->label(value(static fn (Event $event) => $event->type->getColumnName(), $this->getOwnerRecord())),
                 Tables\Columns\ToggleColumn::make('approved'),
